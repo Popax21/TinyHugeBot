50c056465cf8f8320f0bff25cecbd7f90c89394f
Popax21 <popa21@popa21.at>
2023-08-15 01:19:48 +0200

    Build HugeBot in Release mode

--- buildTinyBot.ps1
+++ buildTinyBot.ps1
@@ -1,4 +1,4 @@
 #We need a clean MyBot.cs for the reference to work
 echo "class MyBot:ChessChallenge.API.IChessBot{public ChessChallenge.API.Move Think(ChessChallenge.API.Board b, ChessChallenge.API.Timer t) => b.GetLegalMoves()[0];}" > TinyBot.cs
-dotnet build HugeBot
+dotnet build HugeBot -c Release
 dotnet run --project BotBuilder -- HugeBot.dll TinyBot.dll TinyBot.cs $args[0]
