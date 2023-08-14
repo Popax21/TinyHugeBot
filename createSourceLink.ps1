eaa5596f6660be0a6081a77b4f108106fab04100
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-08-14 18:44:04 -0400

    fix source link creator for powershell

--- createSourceLink.ps1
+++ createSourceLink.ps1
@@ -1,4 +1,1 @@
-Push-Location "Chess-Challenge/Chess-Challenge/src/My Bot"
-Remove-Item MyBot.cs
-New-Item -ItemType SymbolicLink -Path ..\..\..\..\TinyBot.cs -Target MyBot.cs
-Pop-Location
+New-Item -ItemType SymbolicLink -Path TinyBot.cs -Target "Chess-Challenge/Chess-Challenge/src/My Bot/MyBot.cs"
