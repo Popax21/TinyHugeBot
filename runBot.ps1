a0ba3f171c292923cda7df13bcef968701ee0875
Popax21 <popa21@popa21.at>
2023-08-15 18:18:50 +0200

    Fix dev setup

--- runBot.ps1
+++ runBot.ps1
@@ -1,4 +1,5 @@
 ./buildTinyBot.ps1 $args[0]
 Push-Location Chess-Challenge/Chess-Challenge
+Copy-Item -Path ../../TinyBot.cs -Destination "src/My Bot/MyBot.cs" -Recurse -Force
 dotnet run
 Pop-Location
