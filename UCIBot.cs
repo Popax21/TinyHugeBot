68dadb35456d8ca92171eed99b34c743ec14e0c7
Popax21 <popa21@popa21.at>
2023-08-17 00:38:49 +0200

    Simple UCI protocol bot

---
+++ UCIBot.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+using ChessChallenge.API;
+
+class MyBot : IChessBot {
+    private readonly Process proc;
+
+    public MyBot() {
+        proc = Process.Start(new ProcessStartInfo("stro") { RedirectStandardInput = true, RedirectStandardOutput = true })!;
+        proc.StandardInput.WriteLine("hi");
+        ReadUntil("uciok");
+        proc.StandardInput.WriteLine("setoption name asm value false");
+        proc.StandardInput.WriteLine("setoption name hash value 224");
+        proc.StandardInput.WriteLine("ucinewgame");
+    }
+
+    public string ReadUntil(string cmd) {
+        while(proc.StandardOutput.ReadLine() is string msg) {
+            if(msg.StartsWith(cmd)) return msg;
+        }
+        throw new Exception();
+    }
+
+    public Move Think(Board board, Timer timer) {
+        proc.StandardInput.WriteLine($"position fen {board.GetFenString()}");
+        if(board.IsWhiteToMove) proc.StandardInput.WriteLine($"go wtime {timer.MillisecondsRemaining} winc {timer.IncrementMilliseconds}");
+        else proc.StandardInput.WriteLine($"go btime {timer.MillisecondsRemaining} binc {timer.IncrementMilliseconds}");
+
+        string bestMove = ReadUntil("bestmove")[8..].Trim();
+        Console.WriteLine($"BEST MOVE: {bestMove}");
+        return new Move(bestMove, board);
+    }
+}
