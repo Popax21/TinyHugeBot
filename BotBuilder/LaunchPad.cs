6e0079772f57751ade6c91dd8093a42b0d068a62
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-09-17 10:09:57 -0400

    use using static to save 1 token

--- BotBuilder/LaunchPad.cs
+++ BotBuilder/LaunchPad.cs
@@ -1,5 +1,6 @@
 using System;
 using ChessChallenge.API;
+using static System.AppDomain;
 
 class MyBot : IChessBot {
     IChessBot TinyBot;
@@ -31,10 +32,10 @@ class MyBot : IChessBot {
             //Load the tiny bot from the assembly
             //We can't just load it and be done with it, because the byte[] overload doesn't add the assembly to the regular load path
             //As such load it whenever any assembly fails to load >:)
-            ResolveEventHandler asmResolveCB = (_, _) => AppDomain.CurrentDomain.Load(asmDataBuf);
-            AppDomain.CurrentDomain.AssemblyResolve += asmResolveCB;
+            ResolveEventHandler asmResolveCB = (_, _) => CurrentDomain.Load(asmDataBuf);
+            CurrentDomain.AssemblyResolve += asmResolveCB;
             TinyBot = (IChessBot) Activator.CreateInstance("B", "<TINYBOTCLASS>").Unwrap();
-            AppDomain.CurrentDomain.AssemblyResolve -= asmResolveCB;
+            CurrentDomain.AssemblyResolve -= asmResolveCB;
         }
         return TinyBot.Think(board, timer);
     }
