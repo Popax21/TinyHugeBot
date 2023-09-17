01b0557e084a263eb9ace7b052c591bb1c0fb1b4
Popax21 <popa21@popa21.at>
2023-09-17 16:03:23 +0200

    More method tinyfication

--- BotBuilder/Program.cs
+++ BotBuilder/Program.cs
@@ -361,15 +361,88 @@ if(!DEBUG) {
     }
 
     void TinfyMethodBody(CilMethodBody body) {
+        CilInstructionCollection instrs = body.Instructions;
+
         //Optimize instructions
-        body.Instructions.OptimizeMacros();
+        instrs.OptimizeMacros();
+
+        //Remove unnecessary casts
+        CilInstruction? lastCast = null;
+        int lastCastSize = 0;
+        foreach(CilInstruction instr in instrs) {
+            int castSize;
+            switch(instr) {
+                case {} when instr.OpCode == CilOpCodes.Conv_I1:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I1:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I1_Un:
+                case {} when instr.OpCode == CilOpCodes.Conv_U1:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U1:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U1_Un:
+                    castSize = 1;
+                    break;
+
+                case {} when instr.OpCode == CilOpCodes.Conv_I2:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I2:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I2_Un:
+                case {} when instr.OpCode == CilOpCodes.Conv_U2:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U2:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U2_Un:
+                    castSize = 2;
+                    break;
+
+                case {} when instr.OpCode == CilOpCodes.Conv_I4:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I4:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I4_Un:
+                case {} when instr.OpCode == CilOpCodes.Conv_U4:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U4:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U4_Un:
+                    castSize = 4;
+                    break;
+
+                case {} when instr.OpCode == CilOpCodes.Conv_I8:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I8:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I8_Un:
+                case {} when instr.OpCode == CilOpCodes.Conv_U8:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U8:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U8_Un:
+                    castSize = 8;
+                    break;
+
+                case {} when instr.OpCode == CilOpCodes.Conv_I:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_I_Un:
+                case {} when instr.OpCode == CilOpCodes.Conv_U:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U:
+                case {} when instr.OpCode == CilOpCodes.Conv_Ovf_U_Un:
+                    castSize = -1;
+                    break;
+
+                case {} when instr.OpCode == CilOpCodes.Nop: continue;
+                default:
+                    lastCast = null;
+                    continue;
+            }
+
+            if(lastCast != null && lastCastSize == castSize) lastCast.ReplaceWithNop();
+            lastCast = instr;
+            lastCastSize = castSize;
+        }
+
+        //Remove base object constructor calls
+        for(int i = 0; i < instrs.Count-1; i++) {
+            if(instrs[i].OpCode != CilOpCodes.Ldarg_0 || instrs[i+1].OpCode != CilOpCodes.Call) continue;
+            if(instrs[i+1].Operand is not IMethodDescriptor calledMethod) continue;
+            if(calledMethod.DeclaringType?.FullName != "System.Object" || calledMethod.Name != ".ctor") continue;
+            instrs[i].ReplaceWithNop();
+            instrs[i+1].ReplaceWithNop();
+        }
 
         //Trim out NOPs
         CilInstructionLabel? nextInstrLabel = null;
-        foreach(CilInstruction instr in body.Instructions.ToArray()) {
+        foreach(CilInstruction instr in instrs.ToArray()) {
             if(instr.OpCode == CilOpCodes.Nop) {
                 //Fixup jumps
-                foreach(CilInstruction jumpInstr in body.Instructions) {
+                foreach(CilInstruction jumpInstr in instrs) {
                     if(jumpInstr.OpCode.OperandType is not CilOperandType.InlineBrTarget and not CilOperandType.ShortInlineBrTarget) continue;
 
                     //Determine and check the jump target
@@ -383,6 +456,7 @@ if(!DEBUG) {
 
                     jumpInstr.Operand = nextInstrLabel ??= new CilInstructionLabel();
                 }
+                instrs.Remove(instr);
             } else if(nextInstrLabel != null) {
                 nextInstrLabel.Instruction = instr;
                 nextInstrLabel = null;
