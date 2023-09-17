0418800df0bbc61b9d9ac0727030dc70eee741e3
Popax21 <popa21@popa21.at>
2023-09-17 17:27:36 +0200

    Save two more launchpad tokens

--- BotBuilder/LaunchPad.cs
+++ BotBuilder/LaunchPad.cs
@@ -11,7 +11,7 @@ class MyBot : IChessBot {
         foreach(decimal dec in new[] {
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> BEGIN ENCODED ASSEMBLY <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
             <TINYASMENCDAT>
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END ENCODED ASSEMBLY <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  END ENCODED ASSEMBLY  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         }) {
             //Get the bits of the decimal
             bits = decimal.GetBits(dec);
@@ -24,8 +24,8 @@ class MyBot : IChessBot {
             }
 
             //Add the 96 bit integer to the buffer
-            for(int i = 0; i < 12; bits[i++ / 4] >>= 8)
-                TinyBot_asmBuf[asmDataBufOff++] = (byte) bits[i / 4];
+            for(int i = 0; i < 96; i += 8)
+                TinyBot_asmBuf[asmDataBufOff++] = (byte) (bits[i / 32] >> i);
 
             //Accumulate two 4 bit scales, then add to the buffer
             accum <<= 4;
@@ -36,7 +36,7 @@ class MyBot : IChessBot {
         //Load the tiny bot from the assembly
         //We can't just load it and be done with it, because the byte[] overload doesn't add the assembly to the regular load path
         //As such load it whenever any assembly fails to load >:)
-        System.ResolveEventHandler asmResolveCB = (_, _) => CurrentDomain.Load(TinyBot_asmBuf); //We can't use a dynamic variable for this because we need it to be a delegate type
+        System.ResolveEventHandler asmResolveCB = (_, _) => CurrentDomain.Load(TinyBot_asmBuf); //We can't use a dynamic variable for the callback because we need it to be a delegate type
         CurrentDomain.AssemblyResolve += asmResolveCB;
         TinyBot_asmBuf = CurrentDomain.CreateInstanceAndUnwrap("B", "<TINYBOTCLASS>");
         CurrentDomain.AssemblyResolve -= asmResolveCB;
