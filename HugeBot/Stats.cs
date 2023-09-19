303e0f7fb13e60c0b546082d61090df2d70d8045
Popax21 <popa21@popa21.at>
2023-09-19 17:01:05 +0200

    Better EBF stat

--- HugeBot/Stats.cs
+++ HugeBot/Stats.cs
@@ -10,16 +10,21 @@ public partial class MyBot {
     private struct StatsTracker {
         public readonly Stopwatch SearchTimer = new Stopwatch();
         public int NumNodes = 0;
-        private int prevNumNodes = 0;
+        private int prevNumNodes = 0, prevPrevNumNodes = 0;
 
         public readonly long ElapsedMs => SearchTimer.ElapsedMilliseconds;
 
         public StatsTracker() {}
 
-        public void StartSearch(int prevNodes = 0) {
-            //Reset counters
+        public void ResetHistory() => NumNodes = prevNumNodes = prevPrevNumNodes = 0;
+
+        public void StartSearch(bool resetHistory = false) {
+            if(resetHistory) ResetHistory();
+
+            //Reset node counter
+            prevPrevNumNodes = prevNumNodes;
+            prevNumNodes = NumNodes;
             NumNodes = 0;
-            prevNumNodes = prevNodes;
 
             //Start the timer
             SearchTimer.Restart();
@@ -30,20 +35,24 @@ public partial class MyBot {
         public void DumpStats(string prefix = "") {
             Console.WriteLine(prefix + $" - nodes: {NumNodes}");
             Console.WriteLine(prefix + $" - NPS: {(NumNodes / SearchTimer.Elapsed.TotalSeconds).ToString("F4", CultureInfo.InvariantCulture)}");
-            if(prevNumNodes != 0) Console.WriteLine(prefix + $" - EBF: {((double) NumNodes / prevNumNodes).ToString("F8", CultureInfo.InvariantCulture)}");
+            if(prevNumNodes != 0) {
+                Console.Write(prefix + $" - EBF: p1 {((double) NumNodes / prevNumNodes).ToString("F8", CultureInfo.InvariantCulture)}");
+                if(prevPrevNumNodes != 0) Console.Write($" p2 {((double) Math.Sqrt(NumNodes / prevPrevNumNodes)).ToString("F8", CultureInfo.InvariantCulture)}");
+                Console.WriteLine();
+            }
         }
     };
 
     private StatsTracker globalStats = new StatsTracker(), depthStats = new StatsTracker();
 
-    private void STAT_StartGlobalSearch() => globalStats.StartSearch();
+    private void STAT_StartGlobalSearch() => globalStats.StartSearch(resetHistory: true);
     private void STAT_EndGlobalSearch(int maxDepth) {
         globalStats.EndSearch();
         Console.WriteLine($"Finished global search in {globalStats.ElapsedMs}ms, reached depth {maxDepth}");
         globalStats.DumpStats();
     }
 
-    private void STAT_StartDepthSearch(int depth) => depthStats.StartSearch(depth > 1 ? depthStats.NumNodes : 0);
+    private void STAT_StartDepthSearch(int depth) => depthStats.StartSearch(resetHistory: depth <= 1);
     private void STAT_EndDepthSearch(int depth, bool didTimeOut) {
         depthStats.EndSearch();
         Console.WriteLine($"> Finished search to depth {depth} in {depthStats.ElapsedMs}ms{(didTimeOut ? " (timeout)" : "")}");
