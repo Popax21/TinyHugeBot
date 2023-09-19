3bac4927be31e6ecd0e884ff85c580464485828c
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-09-19 09:25:38 -0400

    writing good commit messages is hard

--- BotTuner/Bots/CSChessBot.cs
+++ BotTuner/Bots/CSChessBot.cs
@@ -2,15 +2,17 @@
 using ChessChallenge.API;
 
 namespace BotTuner.Bots {
-    
+
     //Takes a MyBot.cs file, compiles it at runtime, and uses that for the bot
     class CSChessBot : IChessBot {
-        public CSChessBot() {
+
+        //Stores the chess bot contained in the source file
+        private readonly IChessBot bot;
+
+        public CSChessBot(string path) {
 
         }
 
-        public Move Think(Board board, Timer timer) {
-            return Move.NullMove;
-        }
+        public Move Think(Board board, Timer timer) => bot.Think(board, timer);
     }
 }
